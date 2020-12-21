72059f5a0d45cfd1c4bd38477f4c01c8c1339998
zanturik <zanturik@gmail.com>
2020-12-21 12:51:51 -0500

    filter test

--- src/app/filter.service.ts
+++ src/app/filter.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import {MessageService} from './message.service';
+import {ServerConnectorService} from './server-connector.service';
+import {Subscription} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilterService {
+  heroes: any;
+  private wsSubscription: Subscription;
+  constructor(
+    private messageService: MessageService,
+    private service: ServerConnectorService
+  ) {
+    this.wsSubscription = this.service.createObservableSocket()
+      .subscribe(
+        data => this.processMessage(data),
+        err => console.log('error'),
+        () => console.log('The observable stream is complete')
+      );
+    const msg = {
+      action: 'getHeroes'
+    };
+    this.service.sendMessage(JSON.stringify(msg));
+  }
 
-  constructor() { }
+  private processMessage(message: string): void {
+    const messageParsed = JSON.parse(message);
+    if (messageParsed.subject === 'heroesList') {
+      this.heroes.push(messageParsed.content);
+    }
+    if (messageParsed.subject === 'newHero') {
+      this.heroes.push(messageParsed.content);
+    }
+    if (messageParsed.subject === 'heroDeleted') {
+      this.heroes.push(messageParsed.content);
+    }
+    console.log('processing ended');
+  }
 }
